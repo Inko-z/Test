7ba7da90e33774e34c0c2201ddeab6c894f2c4e6
Inko-z <rodolfoandresgp@hotmail.com>
2023-11-23 16:38:11 -0700

    Finished defining data mems for all classes

---
+++ seat.h
@@ -0,0 +1,11 @@
+#include<vector>
+#include <string>
+
+class Seat {       
+  public:  
+   
+  private:          
+    int row;
+    char column;
+    int seat_status; //use for bool logic. 0=unavailable, 1 = available;
+};
