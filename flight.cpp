35a6f1688ab1bb09cb0656c438adecfc073e9a83
sebastian-nieto <sebastian.nieto@ucalgary.ca>
2023-11-23 20:44:31 -0700

    Created basic class implemenations for all functions but the linked list

--- flight.cpp
+++ flight.cpp
@@ -1,4 +1,4 @@
-#include <flight.h>
+#include<flight.h>
 //Default ctor
 Flight::Flight(){
     number_of_rows = 0;
@@ -8,15 +8,32 @@ Flight::Flight(){
     FSmap = {};
 }
 //custom ctor
-Flight::Flight(){
-    number_of_rows = 0;
-    number_of_columns = 0;
-    flight_id = "";
-    pass_listH = nullptr;
-    FSmap = {};
-
-
-
-
+Flight::Flight(int rows, int columns, const std::string& id, PassengerList* passList, const SeatMap& map){
+    number_of_rows = rows;
+    number_of_columns = columns;
+    flight_id = id;
+    pass_listH = passList;
+    FSmap = map;
+}
+//copy ctor
+Flight::Flight(const Flight& rhs){
+    number_of_rows = rhs.number_of_rows;
+    number_of_columns = rhs.number_of_columns;
+    flight_id = rhs.flight_id;
+    if (rhs.pass_listH != nullptr) { //linked list copy
+        pass_listH = new PassengerList(*rhs.pass_listH);
+    } else {
+        pass_listH = nullptr;
+    }
+    FSmap = rhs.FSmap;
+}  
 //implementation
 int Flight::get_number_of_rows()const{return number_of_rows;}
+int Flight::get_number_of_columns()const{return number_of_columns;}
+std::string Flight::get_flight_id()const{return flight_id;}
+SeatMap Flight::get_FSmap() const {return FSmap;}
+void Flight::set_number_of_rows(int r){number_of_rows=r;}
+void Flight::set_number_of_columns(int c){number_of_columns=c;}
+void Flight::set_flight_id(std::string f_id){flight_id=f_id;}
+void Flight::set_FSmap(const SeatMap& fmap){FSmap = fmap;}
+//getter and setter for pass_listh not completed
--- flight.h
+++ flight.h
@@ -13,8 +13,17 @@ class Flight {
   public:  
     // .checkseatavailability
     Flight();
+    Flight::Flight(int rows, int columns, const std::__cxx11::string &id, PassengerList *passList, const SeatMap &map);
+    Flight(const Flight& rhs);
     int get_number_of_rows()const;
     int get_number_of_columns()const;
+    std::string get_flight_id()const;
+    SeatMap get_FSmap()const;
+    void set_number_of_columns(int c);
+    void set_number_of_rows(int r);
+    void set_flight_id(std::string f_id);
+    void set_FSmap(const SeatMap& fmap);
+
 
   private:          
     std::string flight_id;
