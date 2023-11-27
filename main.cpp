17b4792ed9eb3dc432193fea702900e6184d8f9a
Inko-z <rodolfoandresgp@hotmail.com>
2023-11-27 12:42:20 -0700

    Created Main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+#include "airline.h"
+#include "airline.cpp"
+#include "flight.h"
+#include "flight.cpp"
+#include "passenger.h"
+#include "passenger.cpp"
+#include "seat.h"
+#include "seat.cpp"
+
+int main(){
+
+}
