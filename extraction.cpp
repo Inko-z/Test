bf7894cfde8b79b019eb10bad4140f9841c10a6e
Inko-z <rodolfoandresgp@hotmail.com>
2023-11-29 10:06:28 -0700

    fixed func descriptions

--- extraction.cpp
+++ extraction.cpp
@@ -12,16 +12,16 @@ string extract_the_line(ifstream stream){
     //then makes the file ptr go to next line
 }
 Airline extract_airline(string flight_info_line){
-    //take string extracted form get_line_line() and extracts airline info 
+    //take string extracted form extract_the_line() and extracts airline info 
     //put info extracted from said airline into new airline object
     //returns an airline object
 }
 Flight* extract_flight(string flight_info_line){
-     //take string extracted form get_line_line() and extracts flight info and creates airline object on the heap
+     //take string extracted form extract_the_line() and extracts flight info and creates airline object on the heap
      //put info into said flight object
      //return address of object
 }
 Passenger* extract_passenger(string passenger_info_line){
-    //take string extracted form get_line_line() and extracts passenger info and creates passenger object on the heap
+    //take string extracted form extract_the_line() and extracts passenger info and creates passenger object on the heap
     //put info into passenger 
 }
