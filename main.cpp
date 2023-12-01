fde57c95a64e323b9abfe56c6661c7351c4af2d8
AnastasiaZ04 <anastasia.zaharia@ucalgary.ca>
2023-11-30 18:21:27 -0700

    started on creation of objects

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 
 #include "airline.cpp"
+#include "extraction.cpp"
 
 void printfirstmessege();
 void printreturnmessege();
@@ -16,8 +17,23 @@ int main(){
     
     //read all data from file and turn it into objects, lists, ect. in the program
     // airline ptr
-    std::string line = extract_the_line(readingstream);
-    
+    std::string line = extract_the_line(readingstream); // reads first line
+
+    Airline a1 = extract_airline(line); //makes airline object based on first line
+    Flight* f1 = extract_flight(line); //makes flight object on the heap based on first line
+
+    FlightList* fl1 = new FlightList; //first flightlist struct 
+    fl1->next = nullptr;
+    fl1->flight_id = f1->get_flight_id();
+    a1.set_list(fl1); // sets first flightlist struct as head of list
+
+    while(!readingstream.eof()){
+        std::string line = extract_the_line(readingstream);
+        Passenger* new_pass = extract_passenger(line);
+        if(f1->pass_listH == nullptr || new_pass.){
+
+        }
+    }
 
     printfirstmessege();
     printreturnmessege();
