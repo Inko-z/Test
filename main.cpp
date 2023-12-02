e63c20edea7ed6680e810ac270f6dd954ca5542f
AnastasiaZ04 <anastasia.zaharia@ucalgary.ca>
2023-12-01 18:57:46 -0700

    f

--- main.cpp
+++ main.cpp
@@ -102,8 +102,10 @@ int main(){
                 }
                 else{
                     std::cout<<"New Data was not saved."<<std::endl;
-                }              
-            }else if (selection == 6)
+                }       
+                pressreturntocontinue();
+                printoptions();       
+            }else if (selection == '6')
             {
                 runprogram = 0;
             }
