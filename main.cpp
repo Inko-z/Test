f0824b5c4f97e909e92d2652b3f39b4569d03ce7
AnastasiaZ04 <anastasia.zaharia@ucalgary.ca>
2023-12-01 18:24:08 -0700

    fixing merge conflict

--- main.cpp
+++ main.cpp
@@ -101,9 +101,7 @@ int main(){
                 }
                 else{
                     std::cout<<"New Data was not saved."<<std::endl;
-                }
-                pressreturntocontinue();
-                printoptions();               
+                }              
 
             }else if (selection == 6)
             {
